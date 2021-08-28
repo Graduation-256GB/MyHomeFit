03be06d49bdc6478ffe9b4b41f0bc3e5f71a0ca3
Sungeun <sungeuni0208@naver.com>
2021-08-28 21:01:17 +0900

    Feat: [drf react 연동 ]

--- backend/frontend/src/pages/MakeYourSet.js
+++ backend/frontend/src/pages/MakeYourSet.js
@@ -14,7 +14,7 @@ import ListBlock from '../components/MakeYourSet/ListBlock';
 import {useAsync} from "react-async"
 
 const loadPost=async()=>{
-    const res=await fetch('http://127.0.0.1:8000/api/users')
+    const res=await fetch('http://127.0.0.1:8000/api/pose')
     if(!res.ok) throw new Error(res)
     return res.json()
 }
