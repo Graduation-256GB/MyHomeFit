ae605fe8df00fa90eb0f5d31c9843ffe81386e57
Sungeun <sungeuni0208@naver.com>
2021-09-26 00:06:43 +0900

    Fix: [makeyourset 오류 수정, login/signup UI 적용]

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -15,18 +15,10 @@ import FitNotes from "./pages/FitNotes";
 import Calender from "./pages/Calender";
 import SideMenubar from "./pages/menu/SideMenubar";
 import './App.css'
-
-import LoginPage from "./pages/LoginPage";	// 추가
-import SignupPage from "./pages/SignupPage";
 import Navbar from "./pages/Navbar";
-
-// import Navbar from './components/Navbar';
-
-import Signup from "./pages/signup";
+import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
-// import new_makeYourSet from "./pages/makeyourset/MakeYourSet";
-// import MakeTest from "./pages/gaok/MakeYourSet"
 
 const App = () => {
 
@@ -43,11 +35,9 @@ const App = () => {
         <Route path="/calender" component={Calender} />
 
         <Route path="/" exact={true} component={Signup} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/signup" component={SignupPage} />
         <Route path="/navbar" component={Navbar} />
 
-        <Route path="/log" component={Login}/>
+        <Route path="/login" component={Login}/>
         {/* <Route path="/new/makeyourset" component={new_makeYourSet}/> */}
         {/* <Route path="/maketest" component={MakeTest}/> */}
       </div>
--- backend/frontend/src/pages/signup.js
+++ backend/frontend/src/pages/signup.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "../css/signup.css"
 import IconFitnotes from "../images/icon_fitnotes.png"
 import IconSignup from "../images/icon_signup.png"
 
 
-const signup = () => {
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [errors, setErrors] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem('token') !== null) {
+      window.location.replace('http://127.0.0.1:8000/makeyourset');
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      email: email,
+      password1: password1,
+      password2: password2
+    };
+
+    fetch('http://127.0.0.1:8000/api/auth/register/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.key) {
+          localStorage.clear();
+          localStorage.setItem('token', data.key);
+          window.location.replace('http://127.0.0.1:8000/makeyourset');
+        } else {
+          setEmail('');
+          setPassword1('');
+          setPassword2('');
+          localStorage.clear();
+          setErrors(true);
+        }
+      });
+  };
     return (
         <div className="signup-container">
-
+      {loading === false && <h1>Signup</h1>}
+      {errors === true && <h2>Cannot signup with provided credentials</h2>}
             <div className="signup-wrapper">
                 <div className="signup-left">
                     <div><label className="signup-welcome">Welcome</label></div>
@@ -20,28 +66,47 @@ const signup = () => {
                         <label className="signup-title-div">/</label>
                         <label className="signup-title-login">Login</label>
                     </div>
-                    <div className="signup-right-content">
-                        <div><label>ID</label></div>
-                        <div><input/></div>
-                        <div><label>Nickname</label></div>
-                        <div><input/></div>
-                        <div><label>E-mail</label></div>
-                        <div><input type="email"/></div>
-                        <div><label>Password</label></div>
-                        <div><input type="password"/></div>
-                        <div><label>Confirm password.</label></div>
-                        <div><input/></div>
-                    </div>
-                    <div className="signup-btn-wrapper">
-                        <div className="signup-btn">
-                            <button className="signup-btn-txt">SignUp</button>
+                    <form onSubmit={onSubmit}>
+                        <div className="signup-right-content">
+                            {/* <div><label>ID</label></div>
+                            <div><input/></div>
+                            <div><label>Nickname</label></div>
+                            <div><input/></div> */}
+                            <div><label htmlFor='email'>Email</label></div>
+                            <div><input
+                                    name='email'
+                                    type='email'
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    required
+                                    />{' '}</div>
+                            <div><label htmlFor='password1'>Password</label> </div>
+                            <div><input
+                                    name='password1'
+                                    type='password'
+                                    value={password1}
+                                    onChange={e => setPassword1(e.target.value)}
+                                    required
+                                    />{' '}</div>
+                            <div><label htmlFor='password2'>Confirm password:</label></div>
+                            <div><input
+                                    name='password2'
+                                    type='password'
+                                    value={password2}
+                                    onChange={e => setPassword2(e.target.value)}
+                                    required
+                                    />{' '}</div>
                         </div>
+                        <div className="signup-btn-wrapper">
+                            <div className="signup-btn">
+                                <input type="submit" value="Signup" className="signup-btn-txt"></input>
+                            </div>
                         </div>
-
+                    </form>
                 </div>
             </div>
         </div>
     );
 };
 
-export default signup;
+export default Signup;
